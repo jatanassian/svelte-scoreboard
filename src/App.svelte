77ec77c3d49a2fc4f11c987354af69c2d31ecf3f
Julien Atanassian <julien.atanassian@gmail.com>
2020-05-15 19:35:49 -0400

    create AddPlayer form component and add it in the App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
 
   let players = [
     {
@@ -20,6 +21,7 @@
 
 <Navbar />
 <div class="container">
+  <AddPlayer />
   {#if players.length === 0}
     <p>No Players</p>
   {:else}
